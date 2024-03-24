acdf8bc5dd7271133bdb74bcec0fd4ebda869899
Francico-B-Campopiano <campopianofrancisco@gmail.com>
2024-03-24 12:44:59 -0300

    airplane

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿class airplane
+{
+
+    public string accent {get; set;} 
+    public string Time_fly {get; set;}
+    public string Company {get; set;}
+    public string Model {get; set;}
+    public string color {get; set;}
+
+    public void Start {get; set;}
+    {    
+    }
+    public void Stop {get; set;}
+
+    public void Accelerate()
+
+}
