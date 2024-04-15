a4ac56bdc0c4d9210a515bdf0d71b2c82ed2bb65
Francico-B-Campopiano <163892385+Francico-B-Campopiano@users.noreply.github.com>
2024-04-15 09:48:45 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,34 @@
-﻿class airplane
+using Microsoft.EntityFrameworkCore;
+///
+///<author>Francisco</author>
+///
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DataConetext>(opts => 
+opts.UseSqlite(builder.Configuration.GetConnectionString
+("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-
-    public string accent {get; set;} 
-    public string Time_fly {get; set;}
-    public string Company {get; set;}
-    public string Model {get; set;}
-    public string color {get; set;}
-
-    public void Start {get; set;}
-    {    
-    }
-    public void Stop {get; set;}
-
-    public void Accelerate()
-
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
